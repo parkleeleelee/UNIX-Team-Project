2045580d53ae2eac458474e81d46a1699058ddb3
park8306 <sunbi7001@naver.com>
2020-11-18 21:53:27 +0900

    Liary v 0.2

--- public/app.js
+++ public/app.js
@@ -1,16 +1,32 @@
-<<<<<<< HEAD
-< script src = "https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js" > < /script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-https: //firebase.google.com/docs/web/setup#available-libraries -->
-    <
-    script src = "https://www.gstatic.com/firebasejs/8.0.1/firebase-analytics.js" > < /script>
 console.log(firebase)
 var provider = new firebase.auth.GoogleAuthProvider();
 
-=======
-console.log(firebase)
-var provider = new firebase.auth.GoogleAuthProvider();
+// 사용자 인증 //
 
->>>>>>> d86f1b262e94b62de5913c58d118586f58b0975d
-login.onclick = () => auth.signInWithPopup(provider);
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('WhenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // 사인 인
+        userDetails.innerHTML = '<h3>안녕하세요 ${user.displayName}!</h3> <p>사용자 아이디 : ${user.uid}</p>';
+    } else {
+        // 사인인 안 했을 때
+        userDetails.innerHTML = '';
+    }
+});
+
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture: imageUrl
+    });
+}
